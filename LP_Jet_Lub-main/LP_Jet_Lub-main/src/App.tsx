a9c9592f2d5086aa3bc665ea18c89ede2432f2ca
ishelp-lab <ishelp.adm@gmail.com>
2025-09-18 17:56:50 -0300

    Update App.tsx

--- LP_Jet_Lub-main/LP_Jet_Lub-main/src/App.tsx
+++ LP_Jet_Lub-main/LP_Jet_Lub-main/src/App.tsx
@@ -1,28 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { WhatsappFloatIcon } from "./components/icons/Whatsapp";
-import { Pallete } from "./components/Pallete";
-import { Footer } from "./components/Sections/Footer";
-import { Header } from "./components/Sections/Header";
-import { ThemeProvider } from "./contexts";
-import { Index } from "./pages/Index";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './contexts'
+import { Header } from './components/Sections/Header'
+import { Footer } from './components/Sections/Footer'
+import { Index } from './pages/Index'
+// import { WhatsappFloatIcon } from './components/icons/Whatsapp' // reativar depois
 
 export const App = () => {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <div className="min-h-screen bg-white dark:bg-black">
-          <Header />
-          <WhatsappFloatIcon />
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/pallete" element={<Pallete />} />
-            <Route path="*" element={<Index />} />
-          </Routes>
-          <Footer />
-        </div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+        {/* <WhatsappFloatIcon /> */}
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   )
 }
-
-
