0e880fe032300ad63950d47f44ab21a8bded62da
ishelp-lab <ishelp.adm@gmail.com>
2025-09-19 14:32:48 -0300

    Update Index.tsx

--- LP_Jet_Lub-main/LP_Jet_Lub-main/src/pages/Index.tsx
+++ LP_Jet_Lub-main/LP_Jet_Lub-main/src/pages/Index.tsx
@@ -11,9 +11,9 @@ export const Index = () => {
   return (
     <>
       <Hero />
+      <About />
       <Services />
       <Benefits />
-      <About />
       <Video />
       <Testimonials />
       <Location />
