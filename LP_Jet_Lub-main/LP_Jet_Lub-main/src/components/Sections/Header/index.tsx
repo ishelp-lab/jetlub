ce43d1f5817a6b7a6cb961f9340b4ce28933089a
ishelp-lab <ishelp.adm@gmail.com>
2025-09-19 14:21:39 -0300

    Update index.tsx

--- LP_Jet_Lub-main/LP_Jet_Lub-main/src/components/Sections/Header/index.tsx
+++ LP_Jet_Lub-main/LP_Jet_Lub-main/src/components/Sections/Header/index.tsx
@@ -18,7 +18,7 @@ export const Header = () => {
         },
         {
             label: 'Serviços',
-            href: '#servicos'
+            href: '#diferenciais'
         },
         {
             label: 'Localização',
